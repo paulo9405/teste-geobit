a1267735fa1881300b3573f98e57cfc98f98f408
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-11-01 10:10:35 -0300

    Criando models Cidada, Estado e Bairro

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Estado, Cidade, Bairro
 
-# Register your models here.
+
+admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Bairro)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,28 @@
 from django.db import models
 
 
+class Estado(models.Model):
+    nome = models.CharField(max_length=100, null=True)
 
+    # class Meta:
+    #     unique_together = ('nome',)
+
+    def __str__(self):
+        return self.nome
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100, null=True)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Bairro(models.Model):
+    nome = models.CharField(max_length=100, null=True)
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE, null=True)
+    numero = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.nome
